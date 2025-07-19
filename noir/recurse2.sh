2f075fbcaf9c01941477a7b1610ec7dadb5557b2
Cal <calum.moore@gmail.com>
2025-07-19 12:57:20 +0100

    fix recurse2 script for workspace cmds

--- noir/recurse2.sh
+++ noir/recurse2.sh
@@ -1,41 +1,32 @@
 set -e
 
-cd utxo
 rm -rf target
 nargo compile
-bb write_vk --oracle_hash keccak -b ./target/utxo.json -o ./target
-cd ..
+bb write_vk --oracle_hash keccak -b ./target/utxo.json -o ./utxo/target
 
-cd agg_test
-rm -rf target
-nargo compile
-bb write_vk --oracle_hash keccak -b ./target/agg_test.json -o ./target
-cd ..
+bb write_vk --oracle_hash keccak -b ./target/agg_test.json -o ./agg_test/target
 
 echo "Generating utxo proof"
 
-cd utxo
-nargo execute
-bb write_vk -b ./target/utxo.json -o ./target --output_format bytes_and_fields
-bb prove -b ./target/utxo.json -w ./target/utxo.gz -k ./target/vk  --output_format bytes_and_fields  -o ./target
-bb verify -k ./target/vk -p ./target/proof -i ./target/public_inputs
-cd ..
+nargo execute --package utxo
+bb write_vk -b ./target/utxo.json -o ./utxo/target --output_format bytes_and_fields
+bb prove -b ./target/utxo.json -w ./target/utxo.gz -k ./utxo/target/vk  --output_format bytes_and_fields  -o ./utxo/target
+bb verify -k ./utxo/target/vk -p ./utxo/target/proof -i ./utxo/target/public_inputs
 
 
-cd agg_test
 # Populate agg_test/Prover.toml with proof vk and public inputs
 TOML_CONTENT="proof="
-TOML_CONTENT+=$(cat ../utxo/target/proof_fields.json)
+TOML_CONTENT+=$(cat ./utxo/target/proof_fields.json)
 TOML_CONTENT+="\n\npublic_inputs="
-TOML_CONTENT+=$(cat ../utxo/target/public_inputs_fields.json)
+TOML_CONTENT+=$(cat ./utxo/target/public_inputs_fields.json)
 TOML_CONTENT+="\n\nverification_key="
-TOML_CONTENT+=$(cat ../utxo/target/vk_fields.json)
+TOML_CONTENT+=$(cat ./utxo/target/vk_fields.json)
 
-rm -f Prover.toml
-echo "$TOML_CONTENT" > Prover.toml
+rm -f ./agg_test/Prover.toml
+echo "$TOML_CONTENT" > ./agg_test/Prover.toml
 
 
-nargo execute
+nargo execute --package agg_test
 bb write_vk -b ./target/agg_test.json -o ./target --output_format bytes_and_fields
 bb prove -b ./target/agg_test.json -w ./target/agg_test.gz -k ./target/vk  --output_format bytes_and_fields  -o ./target
 bb verify -k ./target/vk -p ./target/proof -i ./target/public_inputs
